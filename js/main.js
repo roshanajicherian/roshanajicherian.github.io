b6be7665ba1f47b67736242dd2a3ab88c24e5895
roshanajicherian <roshanaji777@gmail.com>
2020-05-17 15:54:01 +0530

    Added javascript for better allignment

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(".navbar-nav>li>a").on("click", function () {
   $(".navbar-collapse").collapse("hide");
 });
+
+// Custom function for proper allignment on link click
+var clickCheck = document.querySelectorAll(".navbar-nav>li>a");
+for (var i = 0; i < clickCheck.length; i++) {
+  if (i != clickCheck.length - 1)
+    clickCheck[i].addEventListener("click", checker);
+}
+function checker() {
+  setTimeout(scroller, 10);
+}
+function scroller() {
+  window.scrollBy(0, -80);
+}
